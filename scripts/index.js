8f854714e92a46a88016022bcedd4ff6b2117d83
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2025-02-07 21:52:24 -0500

    add counter

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,13 @@
 const d = document,
+  w = window,
   $modalForget = d.querySelector('.modal-forget-container'),
-  $body = d.querySelector('body')
+  $body = d.querySelector('body'),
+  $messageSuccessForget = d.querySelector('.message-success'),
+  $forgetQuery = d.getElementById('forget-query'),
+  $forgetHiddenElements = d.querySelectorAll(
+    '.modal-forget :not(.message-success)'
+  ),
+  $modalForgetContent = d.querySelector('.modal-forget')
 
 function forgetClicked() {
   $modalForget.classList.toggle('hidden')
@@ -12,12 +19,42 @@ function showInterface() {
   }, 300)
 }
 
+function forgetQueryClicked() {
+  $messageSuccessForget.textContent = ''
+  if ($forgetQuery.value.length === 0) {
+    $messageSuccessForget.textContent =
+      'Ingresa tu correo para recuperar tus credenciales'
+  } else {
+    let elementsHidden = Array.from($forgetHiddenElements),
+      count = 5
+
+    $modalForgetContent.classList.toggle('modal-forget-hidden-elements')
+    elementsHidden.forEach((hidden) => {
+      hidden.classList.toggle('hidden')
+    })
+
+    $messageSuccessForget.classList.toggle('message-success-ok')
+    let interval = setInterval(() => {
+      if (count == 0) {
+        w.location.reload()
+        clearInterval(interval)
+      }
+      $messageSuccessForget.textContent = `Se ha enviado a tu correo: '${$forgetQuery.value}' tu usuario y una clave temporal ✅ ${count}(s)`
+      count--
+    }, 1000)
+  }
+}
+
 d.addEventListener('click', (e) => {
   if (
     e.target.matches('#forget') ||
     e.target.matches('.btn-group-modal small')
   ) {
     forgetClicked()
+  }
+  if (e.target.matches('#query-btn')) {
+    e.preventDefault()
+    forgetQueryClicked()
   }
 })
 
